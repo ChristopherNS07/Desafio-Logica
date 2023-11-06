1f12f1d089b4203e0224a39c7963fa7e447fa3b2
ChristopherNS07 <paulchrisns@gmail.com>
2023-11-05 21:29:07 -0300

    Projeto criação de classe para um jogo

--- Classes_de_um_jogo.js
+++ Classes_de_um_jogo.js
@@ -71,7 +71,7 @@ class game{
         console.log(`O herói ${this.nome} da classe ${this.classe} atacou usando ${tipoAtaque}`)
     }
 }
-// Istancia criada para poder chamar o metodo da nossa classe a seguir
+// Instância criada para poder chamar o metodo da nossa classe a seguir
 let resultado = new game('Arthur',19,'mago')
 
 resultado.ataque()
